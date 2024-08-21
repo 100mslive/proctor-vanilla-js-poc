47ed894fd1e58b4c9343ce73847cb4c9f07d4ab9
amar-1995 <amar@100ms.live>
2024-08-21 14:02:15 +0530

    fix: added vite build

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'proctor.html',
+            dest: '.'
+          },
+        ],
+      }),
+    ]
+});
